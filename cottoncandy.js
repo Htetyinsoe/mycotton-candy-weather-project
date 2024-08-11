7d0544feba6e4be52e135f52fb970c0cabf122e6
Htetyinsoe <htetyysoe@gmail.com>
2024-08-10 22:57:27 -0700

    input axios, take api data, and display temperature

--- cottoncandy.js
+++ cottoncandy.js
@@ -1,8 +1,23 @@
+function refreshWeather(response) {
+  let temperature = document.querySelector("#current-temperature");
+  let city = response.data.city;
+  let temperatureElement = response.data.temperature.current;
+  let h1 = document.querySelector("#weather-app-city");
+  h1.innerHTML = city;
+  temperature.innerHTML = Math.round(temperatureElement);
+}
+
+function getCity(city) {
+  let apiKey = "7fc062ate0o7d407f4902a3f2bcc6bac";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function displayCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let h1 = document.querySelector("#weather-app-city");
-  h1.innerHTML = searchInput.value;
+
+  getCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
